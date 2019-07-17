1a812860d038098e088a96b941c8ad0ff6fe450f
Vanessa Sales <vanessa.moreira@uniriotec.br>
2019-07-16 22:13:13 -0300

    Updates

---
+++ precache-manifest.900210fea011ea61fd6035b89a705772.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e492a4dd3791f15bdf28e60842ea2cfa",
+    "url": "/zeedog/index.html"
+  },
+  {
+    "revision": "36ef0601d0fb62f9064c",
+    "url": "/zeedog/static/css/main.61f9d05c.chunk.css"
+  },
+  {
+    "revision": "625e517f677594679dd9",
+    "url": "/zeedog/static/js/2.7a9f15ff.chunk.js"
+  },
+  {
+    "revision": "36ef0601d0fb62f9064c",
+    "url": "/zeedog/static/js/main.7327ac64.chunk.js"
+  },
+  {
+    "revision": "743e355d5e57e9124641",
+    "url": "/zeedog/static/js/runtime~main.0991f216.js"
+  }
+]);
